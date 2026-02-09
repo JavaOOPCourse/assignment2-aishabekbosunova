8b596511f4bb11feaea5bb45589cdb30247a134d
aishabekbosunova <aishabekbosunova@gmail.com>
2026-02-09 17:28:08 +0600

    update class book

--- src/Book.java
+++ src/Book.java
@@ -9,44 +9,55 @@ public class Book {
     // TODO: Implement parameterized constructor
     public Book(String title, String author, int year) {
         this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isAvailable = true;
     }
 
     // TODO: Implement copy constructor
     public Book(Book other) {
-        // implement
+        this.title = other.title;
+        this.author = other.author;
+        this.year = other.year;
+        this.isAvailable = other.isAvailable;
     }
 
     // TODO: Implement getters
     public String getTitle() {
-        return null;
+        return title;
     }
 
     public String getAuthor() {
-        return null;
+        return author;
     }
 
     public int getYear() {
-        return 0;
+        return year;
     }
 
     public boolean isAvailable() {
-        return false;
+        return isAvailable;
     }
 
     // TODO: Implement borrow logic
     public void borrowBook() {
-        // implement
+        if (isAvailable) {
+            isAvailable = false;
+        } else {
+            System.out.println("Книга уже забронирована.");
+        }
     }
 
     // TODO: Implement return logic
     public void returnBook() {
-        // implement
+        isAvailable = true;
     }
 
     // TODO: Override toString()
     @Override
     public String toString() {
-        return "";
+
+        return "Title: " + title + ", Author: " + author + ", Year: " + year + ", isAvailable" + isAvailable;
     }
 
     // TODO: Override equals()
